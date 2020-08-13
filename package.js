40adc7706adb5e20531e508ea33e6de5c7fd8257
zodern <zodern@users.noreply.github.com>
2020-08-13 10:45:01 -0500

    Add git url to package metadata

--- package.js
+++ package.js
@@ -2,6 +2,7 @@ Package.describe({
   summary: 'Build plugin to provide list of packages and their versions used by the app',
   version: '0.1.0',
   name: 'zodern:meteor-package-versions',
+  git: 'https://github.com/zodern/meteor-package-versions.git'
 });
 
 Package.registerBuildPlugin({
@@ -15,4 +16,4 @@ Package.onUse(api => {
   api.use('isobuild:compiler-plugin@1.0.0')
   api.versionsFrom('METEOR@1.4');
   api.imply('modules');
-})
+});
